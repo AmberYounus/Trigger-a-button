f1948a249dacdf83e1af055f458586395504f479
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-05-29 10:30:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+function welcome() {
+  alert("Sunshine is a welcome thing. It brings a lot of brightness");
+}
+
+var input = document.getElementById("myInput");
+input.addEventListener("keypress", function (event) {
+  if (event === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
 
